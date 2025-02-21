78c44ba635bea972c923ba03112763a0f683563e
Daniel Soares <daniel.soares@me.com>
2025-02-21 09:46:40 -0300

    fix: tailwind config (#89)
    * fix: tailwind config
    
    * fix: types
    
    * chore: details
    
    * chore: details
    
    * chore: details

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router'
 
-export default function RootLayout() {
+export default function RootLayout(): React.JSX.Element {
   return <Stack screenOptions={{ headerShown: false }} />
 }
