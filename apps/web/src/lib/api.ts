4fa203fc1bb79581f31ab3698925b8d27503c7ad
Daniel Soares <daniel.soares@me.com>
2025-03-21 12:35:26 -0300

    feat: add error retry plugin

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,6 +1,7 @@
 import 'server-only'
 
 import xior from 'xior'
+import errorRetryPlugin from 'xior/plugins/error-retry'
 
 import { env } from '@/config/env'
 import { getSessionAction } from '@/modules/session/actions/get-session.action'
@@ -8,8 +9,19 @@ import { getSessionAction } from '@/modules/session/actions/get-session.action'
 const api = xior.create({
   baseURL: env.SERVER_URL,
   cache: 'force-cache',
-  next: { revalidate: 60 }
+  next: { revalidate: 60 },
+  timeout: 5000
 })
+
+api.plugins.use(
+  errorRetryPlugin({
+    retryTimes: 3,
+    retryInterval: 1000,
+    enableRetry(_, error): boolean | undefined {
+      if ([400, 401, 403].includes(error.response?.status ?? 0)) return false
+    }
+  })
+)
 
 api.interceptors.request.use(async config => {
   const { accessToken, hasValidSession } = await getSessionAction()
